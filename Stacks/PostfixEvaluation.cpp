88f42a29aeccd61b82f7e6a4104be0a5f31c4c21
Kunal Latwal <163636801+KunalLatwal@users.noreply.github.com>
2026-01-05 15:37:21 +0530

    Add PostfixEvaluation class for RPN evaluation
    Implement postfix evaluation using a stack.

---
+++ Stacks/PostfixEvaluation.cpp
@@ -0,0 +1,23 @@
+
+Concept: Stack (postfix evaluation)
+Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(auto t:tokens){
+            if(t=="+"||t=="-"||t=="*"||t=="/"){
+                int b=st.top(); st.pop();
+                int a=st.top(); st.pop();
+                if(t=="+") st.push(a+b);
+                else if(t=="-") st.push(a-b);
+                else if(t=="*") st.push(a*b);
+                else st.push(a/b);
+            }else{
+                st.push(stoi(t));
+            }
+        }
+        return st.top();
+    }
+};
