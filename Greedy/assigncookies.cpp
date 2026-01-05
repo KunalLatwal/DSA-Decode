50471cc557a379e7e9307cfacdd3a1d204730524
Kunal Latwal <163636801+KunalLatwal@users.noreply.github.com>
2026-01-05 15:52:42 +0530

    Create assigncookies.cpp

---
+++ Greedy/assigncookies.cpp
@@ -0,0 +1,22 @@
+
+🔗 LeetCode 455
+https://leetcode.com/problems/assign-cookies/
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i=0, j=0, cnt=0;
+        while(i<g.size() && j<s.size()){
+            if(s[j] >= g[i]){
+                cnt++;
+                i++; j++;
+            } else j++;
+        }
+        return cnt;
+    }
+};
+
+
+};
