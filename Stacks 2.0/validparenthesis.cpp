89bbff8c60cba5a9b1f4d9eeb69d26b9ff4574a2
Kunal Latwal <163636801+KunalLatwal@users.noreply.github.com>
2026-01-06 07:56:21 +0530

    Create validparenthesis.cpp

---
+++ Stacks 2.0/validparenthesis.cpp
@@ -0,0 +1,24 @@
+🔗 https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s) {
+            if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+            else {
+                if(st.empty()) return false;
+                if((c==')' && st.top()!='(') ||
+                   (c=='}' && st.top()!='{') ||
+                   (c==']' && st.top()!='['))
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
+
+🧠 learning: matching + LIFO
