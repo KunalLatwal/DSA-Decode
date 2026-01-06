1da7cfb2cc27c6d6ef81784114c0b02741ba7eef
Kunal Latwal <163636801+KunalLatwal@users.noreply.github.com>
2026-01-06 07:57:34 +0530

    Create dailytemperature.cpp

---
+++ Stacks 2.0/dailytemperature.cpp
@@ -0,0 +1,22 @@
+🔗 https://leetcode.com/problems/daily-temperatures/
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n=temp.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+
+        for(int i=0;i<n;i++) {
+            while(!st.empty() && temp[i]>temp[st.top()]) {
+                int idx=st.top(); st.pop();
+                ans[idx]=i-idx;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
+
+
+🧠 learning: index stack
