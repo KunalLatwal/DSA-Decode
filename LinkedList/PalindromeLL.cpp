5272a30d22f640d8d0b6448f9899f9bc2c41d5c1
Kunal Latwal <163636801+KunalLatwal@users.noreply.github.com>
2026-01-09 19:54:18 +0530

    Create PalindromeLL.cpp

---
+++ LinkedList/PalindromeLL.cpp
@@ -0,0 +1,36 @@
+https://leetcode.com/problems/palindrome-linked-list/
+
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        while(head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = reverse(slow);
+        ListNode* first = head;
+
+        while(second) {
+            if(first->val != second->val)
+                return false;
+            first = first->next;
+            second = second->next;
+        }
+        return true;
+    }
+};
