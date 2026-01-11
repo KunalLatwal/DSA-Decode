1508137db6ae5cc75f9bde44a12a964ac0f07cb3
Kunal Latwal <163636801+KunalLatwal@users.noreply.github.com>
2026-01-11 17:51:53 +0530

    Create reverselinkedlist.cpp

---
+++ LinkedList 3.0/reverselinkedlist.cpp
@@ -0,0 +1,26 @@
+https://leetcode.com/problems/reverse-linked-list-ii/
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head || left == right) return head;
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+
+        for(int i = 1; i < left; i++)
+            prev = prev->next;
+
+        ListNode* curr = prev->next;
+
+        for(int i = 0; i < right - left; i++) {
+            ListNode* next = curr->next;
+            curr->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
+        }
+
+        return dummy->next;
+    }
+};
