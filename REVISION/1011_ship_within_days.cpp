786862b0ca7fa75af837ba33615bfc21d332647c
Kunal Latwal <163636801+KunalLatwal@users.noreply.github.com>
2026-01-16 22:02:27 +0530

    Update 1011_ship_within_days.cpp

--- REVISION/1011_ship_within_days.cpp
+++ REVISION/1011_ship_within_days.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    bool ok(vector<int>& w,int d,int cap){
+    bool valid(vector<int>& w,int d,int cap){
         int days=1,load=0;
         for(int x:w){
             if(x>cap) return false;
@@ -17,7 +17,7 @@ public:
         int l=1,r=accumulate(w.begin(),w.end(),0),ans=r;
         while(l<=r){
             int m=l+(r-l)/2;
-            if(ok(w,d,m)) ans=m,r=m-1;
+            if(valid(w,d,m)) ans=m,r=m-1;
             else l=m+1;
         }
         return ans;
