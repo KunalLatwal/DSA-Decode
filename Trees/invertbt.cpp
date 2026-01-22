295427bf715c89818d8812c73990894356df879a
Fastcommiter <163636801+fastcommiter@users.noreply.github.com>
2026-01-22 23:16:00 +0530

    Create invertbt.cpp

---
+++ Trees/invertbt.cpp
@@ -0,0 +1,17 @@
+
+🔗 https://leetcode.com/problems/invert-binary-tree/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+
+        root->left = right;
+        root->right = left;
+
+        return root;
+    }
+};
