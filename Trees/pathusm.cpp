c83d058a924afe0f98295e4f40f39c50069afd9b
Fastcommiter <163636801+fastcommiter@users.noreply.github.com>
2026-01-22 23:16:52 +0530

    Create pathusm.cpp

---
+++ Trees/pathusm.cpp
@@ -0,0 +1,15 @@
+
+🔗 https://leetcode.com/problems/path-sum/
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+
+        if(!root->left && !root->right)
+            return targetSum == root->val;
+
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
