54f3dded0f9df725d1be67dd829fe38a9a56ceba
Fastcommiter <163636801+fastcommiter@users.noreply.github.com>
2026-01-22 23:16:35 +0530

    Create ismirror.cpp

---
+++ Trees/ismirror.cpp
@@ -0,0 +1,19 @@
+
+🔗 https://leetcode.com/problems/symmetric-tree/
+
+class Solution {
+public:
+    bool check(TreeNode* a, TreeNode* b) {
+        if(!a && !b) return true;
+        if(!a || !b) return false;
+        if(a->val != b->val) return false;
+
+        return check(a->left, b->right) &&
+               check(a->right, b->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return check(root->left, root->right);
+    }
+};
