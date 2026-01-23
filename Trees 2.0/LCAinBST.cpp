e7a2a3421242eaab927185c027115f332686701f
Fastcommiter <163636801+fastcommiter@users.noreply.github.com>
2026-01-23 13:04:02 +0530

    Create LCAinBST.cpp

---
+++ Trees 2.0/LCAinBST.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+
+        if(p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+
+        if(p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+
+        return root;
+    }
+};
