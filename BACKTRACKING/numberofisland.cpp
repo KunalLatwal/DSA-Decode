c2fe530fb51602beaf57adb0c2f2392cf276ee11
Fastcommiter <163636801+fastcommiter@users.noreply.github.com>
2026-02-06 21:25:02 +0530

    Create numberofisland.cpp

---
+++ BACKTRACKING/numberofisland.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/number-of-islands/
+
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        int r=grid.size(), c=grid[0].size();
+        if(i<0||i>=r||j<0||j>=c||grid[i][j]=='0') return;
+        grid[i][j]='0';
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int cnt=0;
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[0].size();j++)
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    dfs(grid,i,j);
+                }
+        return cnt;
+    }
+};
