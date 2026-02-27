30660bd3a5dd9e2672f1dbedc969877c2ef6e173
Fastcommiter <163636801+fastcommiter@users.noreply.github.com>
2026-02-27 11:08:47 +0530

    Add MedianFinder class for streaming median calculation
    Implemented a MedianFinder class that maintains a stream of numbers and can return the median efficiently.

---
+++ P_Queue/medianofadatastream.cpp
@@ -0,0 +1,40 @@
+class MedianFinder {
+public:
+    priority_queue<int>maxh;
+    priority_queue<int,vector<int>,greater<>>minh;
+    MedianFinder() {        
+    }
+    void addNum(int num) {
+        //maxheap mai jb add krro ki tbh num chota ho ya equal
+        //minheap mai tbh dalo tbh num >= minheap.top();
+        if(maxh.empty() || num <= maxh.top())
+        {
+            maxh.push(num);
+        }
+        else{
+            minh.push(num);
+        }
+
+        if(maxh.size() > minh.size()+1)
+        {
+            minh.push(maxh.top());
+            maxh.pop();
+        }
+        else if(minh.size() > maxh.size()+1)
+        {
+            maxh.push(minh.top());
+            minh.pop();
+        }
+    }
+    double findMedian() {
+        if(maxh.size()==minh.size()) return (minh.top()+maxh.top())/2.0;
+        else if(maxh.size() > minh.size()) return maxh.top();
+        return minh.top();
+    }
+};
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
